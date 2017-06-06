9e6a3a838ffba8e3394200a090456c35b31a6647
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2017-06-06 19:27:35 +0200

    Simplified main file even more

--- need-hug/cpp/Main.cpp
+++ need-hug/cpp/Main.cpp
@@ -1,5 +1,6 @@
 #include <need-hug-lib/hpp/NeedHugContext.hpp>
 #include <need-hug-lib/hpp/ReturnCode/ReturnCodeConverter.hpp>
+
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -8,20 +9,19 @@ int main(int argc, char** argv)
 	ReturnCode returnCode = ReturnCode::Continue;
 	while(returnCode == NeedHug::ReturnCode::Continue)
 	{
+		// When the code reaches here, nothing should be allocated any longer in order to avoid memoryleaks.
+		NeedHugContext needHugContext;
+		try
 		{
-			NeedHugContext needHugContext;
-			try
-			{
-				returnCode = needHugContext.Start();
-			}
-			catch(...)
-			{
-				std::cout << "Game crashed unexpectedly" << std::endl;
-			}
+			returnCode = needHugContext.Start();
 		}
-		// When the code reaches here, nothing should be allocated anylonger in order to avoid memoryleaks.
+		catch(...)
+		{
+			std::cout << "Game crashed unexpectedly" << std::endl;
+			returnCode = ReturnCode::Unknown;
+		}
 	}
-
+	
 	std::cout << "The game returned the following stopCode '" << ReturnCodeConverter().Convert(returnCode).info << "'." << std::endl;
 	int returnCodeValue = 1;
 
@@ -29,6 +29,5 @@ int main(int argc, char** argv)
 	{
 		returnCodeValue = 0;
 	}
-
 	return returnCodeValue;
 }
