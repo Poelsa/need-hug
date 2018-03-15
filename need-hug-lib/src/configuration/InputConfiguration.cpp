21943172653678977a0c07803322deb4f30ee0a2
Poelsa <joel.begnert@gmail.com>
2018-03-15 19:11:39 +0100

    Nu har vi mer koll

--- need-hug-lib/src/configuration/InputConfiguration.cpp
+++ need-hug-lib/src/configuration/InputConfiguration.cpp
@@ -5,7 +5,12 @@ namespace NeedHug
 {
     ButtonActionDefinition InputConfiguration::DefaultInputConfig(ActionType actionType, PlayerId playerId)
     {
-        std::string buttonString = ConfigurationManager::GetValue(actionType.toString() + "_" + playerId.toString());
-        StringToButton(buttonString); //Hur konverteras knappsträngen till ButtonActionDefinition?
+        std::string buttonString = ConfigurationManager::GetValue(actionType.toString() + "_" + playerId.toString() + "_key");
+        int buttonConstraint = ConfigurationManager::GetValue(actionType.toString() + "_" + playerId.toString() + "_constraint");
+        StringToActiveButtonCallback(buttonString); 
+        EnumToActionCallback(actionType, playerId);
+        IntToContraintCallback(buttonConstraint);
     }
+
+    // string till activebuttoncallback
 }
